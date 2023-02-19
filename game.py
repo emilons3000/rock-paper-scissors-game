123ec88cc703abed14943c73373fe7d9255d927c
emilons3000 <124282284+emilons3000@users.noreply.github.com>
2023-02-19 18:02:01 +0200

    Now we save history of player turns

--- game.py
+++ game.py
@@ -1,13 +1,25 @@
 import random
 turns = ['rock', 'paper', 'scissors']
+human_turns = []  #tuksi sarkasti kuriem append pievieno izveles
+computer_turns = []
+
 
 while(True):
-    human_turn = input("Enter your turn or type exit:")
+    human_turn = input("Enter your turn or type exit:") #abi izvelas izveli
     computer_turn = random.choice(turns)
 
     if human_turn == 'exit':
         print('thank you for playing')
         break
+        
+
+    human_turns.append(human_turn) #saskaita izveles un pievieno tuksajam sarkastam
+    computer_turns.append(computer_turn)
+
+
+
+
+    
 
     print(f'Human:{human_turn} vs. Computer: {computer_turn}')
     if human_turn == computer_turn:
